21fc269e7a27a902aef4918e88579bf88e797607
Juliano Bittencourt <julianobittencourteprado@gmail.com>
2019-07-25 11:34:45 -0400

    super commit

--- js/Controller/MenuControler.js
+++ js/Controller/MenuControler.js
@@ -1,3 +1,17 @@
-function abrirMenu(){
-    document.getElementById("menu")
+function openDrawer() {
+    document.querySelector('.drawer-menu').style.animation = 'abrir-menu .5s ease-in'
+    document.querySelector('.drawer').style.display = 'flex'
+    document.querySelector('.drawer').style.animation = 'escurecer-div .5s'
+    document.querySelector('body').style.overflowY = 'hidden'
+
+}
+
+function closeDrawer() {
+    document.querySelector('.drawer-menu').style.animation = 'fechar-menu .5s ease'
+    document.querySelector('.drawer').style.animation = 'clarear-div .5s'
+    document.querySelector('body').style.overflowY = 'auto'
+    setTimeout(() => {
+        document.querySelector('.drawer').style.display = 'none'
+    }, 500)
+
 }
